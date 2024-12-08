ce2f1baaf28d14b7360ec5fb87fbe7382276df13
Lucas Teixeira <teixeirajob.one@gmail.com>
2024-12-08 15:57:19 -0300

    updated POST movie return format

--- src/main/java/com/dravinck/movies/service/MovieService.java
+++ src/main/java/com/dravinck/movies/service/MovieService.java
@@ -1,10 +1,13 @@
 package com.dravinck.movies.service;
 
+import com.dravinck.movies.entity.Category;
 import com.dravinck.movies.entity.Movie;
+import com.dravinck.movies.entity.Streaming;
 import com.dravinck.movies.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,12 +16,16 @@ import java.util.Optional;
 public class MovieService {
 
     private final MovieRepository repository;
+    private final CategoryService categoryService;
+    private final StreamingService streamingService;
 
     public List<Movie> findAll(){
         return repository.findAll();
     }
 
     public Movie save(Movie movie){
+        movie.setCategories(this.findCategories(movie.getCategories()));
+        movie.setStreamings(this.findStreaming(movie.getStreamings()));
         return repository.save(movie);
     }
 
@@ -28,5 +35,17 @@ public class MovieService {
 
     public void delete (Long id){
         repository.deleteById(id);
+    }
+
+    private List<Category> findCategories(List<Category> categories){
+        List<Category> categoriesFound = new ArrayList<>();
+        categories.forEach(category -> categoryService.findById(category.getId()).ifPresent(categoriesFound::add));
+        return categoriesFound;
+    }
+
+    private List<Streaming> findStreaming(List<Streaming> streamings){
+        List<Streaming> streamingsFound = new ArrayList<>();
+        streamings.forEach(streaming -> streamingService.findById(streaming.getId()).ifPresent(streamingsFound::add));
+        return streamingsFound;
     }
 }
