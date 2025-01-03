b7003f52b8f58c4d60e1410a899f9f7db505d65b
Lucas Teixeira <teixeirajob.one@gmail.com>
2025-01-02 22:18:29 -0300

    created User

---
+++ src/main/java/com/dravinck/movies/entity/User.java
@@ -0,0 +1,27 @@
+package com.dravinck.movies.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+
+}
