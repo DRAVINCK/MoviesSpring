17af05770d759592e13d2767823a0efa7d8afe60
Lucas Teixeira <teixeirajob.one@gmail.com>
2025-01-02 22:17:40 -0300

    created userService

---
+++ src/main/java/com/dravinck/movies/service/UserService.java
@@ -0,0 +1,12 @@
+package com.dravinck.movies.service;
+
+import com.dravinck.movies.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository repository;
+}
