b70d34e68ac6dc692be39a57df6b8e2030cf1155
Lucas Teixeira <teixeirajob.one@gmail.com>
2025-01-09 22:54:41 -0300

    token JWT

--- src/main/java/com/dravinck/movies/config/SecurityFilter.java
+++ src/main/java/com/dravinck/movies/config/SecurityFilter.java
@@ -6,10 +6,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.util.Strings;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.security.Security;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -26,7 +31,12 @@ public class SecurityFilter extends OncePerRequestFilter {
 
             String token = authorizationHeader.substring("Bearer ".length());
 
-            tokenService .validateToken(token);
+            Optional<JWTUserData> OptjwtUserData = tokenService.verifyToken(token);
+            if (OptjwtUserData.isPresent()) {
+                JWTUserData userData = OptjwtUserData.get();
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userData, null, null);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
 
             filterChain.doFilter(request, response);
         } else {
