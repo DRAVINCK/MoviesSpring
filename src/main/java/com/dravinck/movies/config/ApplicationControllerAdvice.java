4ecfec7c299c186be9fee6244303a1d0c48b0a85
Lucas Teixeira <teixeirajob.one@gmail.com>
2025-01-11 19:22:04 -0300

    Created ApplicationControllerAdvice

---
+++ src/main/java/com/dravinck/movies/config/ApplicationControllerAdvice.java
@@ -0,0 +1,32 @@
+package com.dravinck.movies.config;
+
+import com.dravinck.movies.exceptions.UsernameOrPasswordinvalidException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(UsernameOrPasswordinvalidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String HandleNotFoundException(UsernameOrPasswordinvalidException e){
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleArgumentNotValidException(MethodArgumentNotValidException e){
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+}
